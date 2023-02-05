9fde62ffcbb23718ca22b503943ffddd033c293f
Katya <kittyserenakat@gmail.com>
2023-02-05 18:15:39 -0500

    tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ import {
 } from "@pankod/refine-chakra-ui";
 import { ChakraUIInferencer } from "@pankod/refine-inferencer/chakra-ui";
 
+import { ProductList } from "pages/products/list";
+
 
 import dataProvider from "@pankod/refine-simple-rest";
 import routerProvider from "@pankod/refine-react-router-v6";
@@ -28,7 +30,7 @@ function App() {
         resources={[
                     {
                         name: "products",
-                        list: ChakraUIInferencer,
+                        list: ProductList,
                         show: ChakraUIInferencer,
                         create: ChakraUIInferencer,
                         edit: ChakraUIInferencer,
